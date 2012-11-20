6f53713be0f0ba1820ebac081f71dd77584d9efe
Dmitry <unknownliveid@hotmail.com>
2012-11-20 01:09:16 -0500

    ie detection, a lot of content

--- js/zombie-script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $("div.zone").hover(function(e) {
-       $("div.zone").removeClass("full");             
-       $(this).removeClass("zone").addClass("full");          
-    }, function (e) {
-        $(this).removeClass("full").addClass("zone");  
-    });
-    
-});
