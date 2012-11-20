3b82122e39a153fa07fbed3cccb028d2b7ebe795
Dmitry <unknownliveid@hotmail.com>
2012-11-20 11:55:52 -0500

    success message

--- add.php
+++ add.php
@@ -1,5 +1,5 @@
 <?php 
 if (isset($_GET['email'])) {
     file_put_contents(__DIR__ . "emails.txt", $_GET['email'] . "\n", FILE_APPEND);
-    header("location: http://" . $_SERVER['HTTP_HOST']);
+    header("location: http://" . $_SERVER['HTTP_HOST'] . "?message=success");
 }
